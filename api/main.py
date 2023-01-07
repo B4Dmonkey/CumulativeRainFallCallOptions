2d989e9e37d8e5e63dff92e07e74ff373bca71cf
josiah <josiah@appstack.nyc>
2023-01-07 11:53:56 -0500

    ground work for db modeling

--- api/main.py
+++ api/main.py
@@ -1,16 +1,49 @@
-def main():
-    return True
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from peewee import SqliteDatabase, Model, DateField, FloatField
+
+load_dotenv()
+
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+
+INDEX = 18  # should be the rainfall
+
+# database = PostgresqlDatabase(DATABASE_NAME, user=DATABASE_USER)
+database = SqliteDatabase("rain.db")
 
 
-INDEX = 18
-LAYER = 8
+class BaseModel(Model):
+    class Meta:
+        database = database  # This model uses the "people.db" database.
 
 
-def payout(strike: float, exit: float, notional: float):
-    spread = max(INDEX - strike, 0)
-    layer = min(LAYER, spread)
+class RainData(BaseModel):
+    date = DateField(null=False, index=True)
+    value = FloatField(null=False)
+
+# * This script is only run once to create the table and seed the db
+# Todo: Ensure that if this is run multiple times duplicates are not entered.
+def create_tables():
+    with database:
+        database.create_tables([RainData])
+        rainDataRaw = pd.read_excel("./NYC_Rain_Example.xlsx")
+        for index, row in rainDataRaw.iterrows():
+            RainData.create(date=row["Date"], value=row["Col1"])
+
+
+def payout(strike: float, exit_: float, notional: float):
+    strikeIndex = max(INDEX - strike, 0)
+    layer = min(exit_ - strike, strikeIndex)
     return layer * notional
 
 
+def rainfall_index(start, end):
+    pass
+
+
 if __name__ == '__main__':
-    main()
+    pass
+    # create_tables()
--- api/test.py
+++ api/test.py
@@ -1,17 +1,15 @@
 import unittest
-from main import main, payout
+from main import payout
 
 
 class TestAPI(unittest.TestCase):
 
     def test_payoutFormula(self):
-        self.assertTrue(main())
-
-    def test_payoutFormula(self):
         strike = 16
-        ex = 0
+        ex = 24
         notional = 25000
         self.assertEqual(payout(strike, ex, notional), 50000)
+
 
 
 if __name__ == '__main__':
